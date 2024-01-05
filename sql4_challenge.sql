e97d07f8af6c63e5b88b22b73c498d7cfc2b7e3f
Geet Tandon <64631428+geettandon@users.noreply.github.com>
2024-01-05 19:44:05 +0530

    Update sql4_challenge.sql

--- sql4_challenge.sql
+++ sql4_challenge.sql
@@ -1,9 +1,6 @@
--- Intro
-/* 
-You are a Finance Analyst working for 'The Big Bank'
-You have been tasked with finding out about your customers and their banking behaviour. 
-Examine the accounts they hold and the type of transactions they make to develop greater insight into your customers.
-*/
+-- Challenge 4 - Financial Analysis
+
+-- QUESTIONS
 
 -- Query the customers table
 SELECT *
